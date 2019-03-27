3e93c9769a26e4f2713359c526828fbae14d1109
LinnOewre <48894694+LinnOewre@users.noreply.github.com>
2019-03-26 21:36:20 -0400

    Update Program.cs

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,7 +6,7 @@ namespace Lab01
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from Linn Oewre!");
         }
     }
 }
